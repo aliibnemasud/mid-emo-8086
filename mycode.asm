c17eb1df38dc84d9d64573bacc2730ddcca9f493
aliibnemasud <mdmasudrony@gmail.com>
2023-03-13 21:16:12 +0600

    emo 8086 main function added

---
+++ mycode.asm
@@ -0,0 +1,31 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+
+.CODE
+    MAIN PROC
+        
+        
+        
+        
+        
+        
+        
+         
+         
+         
+         
+        MOV AH , 4CH
+                
+        INT 21H
+        
+        MAIN ENDP
+    
+    
+    
+    END MAIN
+        
+        
+        
+
+        
